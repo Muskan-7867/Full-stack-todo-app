2f346af74f614839fa39ba6d430b9599b52978ab
muskan <muskanloach984@gmail.com>
2024-09-27 15:18:08 +0530

    send mail api

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,7 +1,7 @@
-
 import { Resend } from 'resend';
 import { EmailTemplate } from 'src/components/Email-template';
 
+// Fetch the API key from process.env
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
@@ -10,7 +10,7 @@ export async function POST() {
       from: 'Acme <onboarding@resend.dev>',
       to: ['delivered@resend.dev'],
       subject: 'Welcome to Todo app!!',
-      react: EmailTemplate({ firstName: 'John' }),
+      react: EmailTemplate({ firstName: 'John' }), 
     });
 
     if (error) {
