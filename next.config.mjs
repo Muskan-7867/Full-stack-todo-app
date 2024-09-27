481829d4c64bacff9d4d3691db78a51464d1861a
muskan <muskanloach984@gmail.com>
2024-09-27 15:18:31 +0530

    config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
+// next.config.js
+module.exports = {
+    env: {
+      RESEND_API_KEY: process.env.RESEND_API_KEY,
+    },
+  };
+  
 export default nextConfig;
